0dbc85a0818add7c9a5a3244c3d34ac7504a8874
Emanuel Eagle <63474634+meagle21@users.noreply.github.com>
2024-01-19 11:28:15 -0500

    Delete extensionDownload.sh

--- extensionDownload.sh
+++
@@ -1,5 +0,0 @@
-rm LinkReaderChromeExtension
-curl -o tempRepo.zip -Lk https://github.com/meagle21/LinkReaderChromeExtension/archive/master.zip
-unzip tempRepo.zip -d LinkReaderChromeExtension
-rm tempRepo.zip
-rm LinkReaderChromeExtension/LinkReaderChromeExtension-main/images/extension.png
