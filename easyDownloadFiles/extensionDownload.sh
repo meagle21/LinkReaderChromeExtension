10156f2612c9133f6b1f4702f7e653abc0b9ad36
Emanuel Eagle <63474634+meagle21@users.noreply.github.com>
2024-01-19 11:31:32 -0500

    Add files via upload

--- easyDownloadFiles/extensionDownload.sh
+++ easyDownloadFiles/extensionDownload.sh
@@ -1,5 +1,6 @@
-rm LinkReaderChromeExtension
+rm -r LinkReaderChromeExtension
 curl -o tempRepo.zip -Lk https://github.com/meagle21/LinkReaderChromeExtension/archive/master.zip
 unzip tempRepo.zip -d LinkReaderChromeExtension
 rm tempRepo.zip
-rm LinkReaderChromeExtension/LinkReaderChromeExtension-main/images/extension.png
+rm LinkReaderChromeExtension/LinkReaderChromeExtension-main/images/extension.png 
+rm -r LinkReaderChromeExtension/LinkReaderChromeExtension-main/easyDownloadFiles
